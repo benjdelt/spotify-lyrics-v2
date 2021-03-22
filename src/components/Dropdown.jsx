b677b1a09b6684d16f156d3ef8060bfd3ddc9857
Ben Deltenre <benjdelt@gmail.com>
2021-03-22 10:15:53 -0700

    closed dropdown on outside click

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,11 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons'
 import '../styles/Dropdown.css';
 
 function Dropdown(props) {
 
+  const wrapperRef = useRef(null);
+
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, false);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, false);
+    };
+  }, [])
+
+  const handleClick = event => {
+    event.stopPropagation();
+    setOpen(!open);
+  };
+
+  const handleClickOutside = event => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      setOpen(false);
+    }
+  };
 
   const closedIcon = props.up ? faCaretUp : faCaretDown;
   const openIcon = props.up ? faCaretDown : faCaretUp;
@@ -22,17 +42,19 @@ function Dropdown(props) {
 
   return (
     <div className="dropdown-parent">
-      <button onClick={() => setOpen(!open)} className="dropdown-label">
+      <button onClick={handleClick} className="dropdown-label">
         {props.children} &nbsp;
         <FontAwesomeIcon icon={open ? openIcon : closedIcon} />
       </button>
-      <div className="dropdown-menu" style={styles}>
+      {/* {open && */}
+      <div className="dropdown-menu" style={styles} ref={wrapperRef}>
         {props.options.map(option => (
           <div key={option} className="dropdown-option">
             <span className="option-text">{option}</span>
           </div>
         ))}
       </div>
+      {/* } */}
     </div>
   )
 }
