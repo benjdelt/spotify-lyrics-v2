0ec9d4d06b8d14fe18e2b25438306883b6bbbf8d
Ben Deltenre <benjdelt@gmail.com>
2021-05-04 22:48:48 -0700

    Removed commented out code

--- src/App.jsx
+++ src/App.jsx
@@ -66,21 +66,6 @@ function App() {
         <Profile user={user} logout={logout} />
         {user.name ? (
           <Song song={song} />
-          // <section className="song">
-          //   <header className="song-header">
-          //     <img src={song.coverUrl} alt="cover" className="cover"/>
-          //     <section className="song-info">
-          //       <h2>{song.title}</h2>
-          //       <h5>{song.artist}</h5>
-          //       <p>{song.album}</p>
-          //     </section>
-          //   </header>
-          //   <section className="song-main">
-          //     <p>
-          //       {song.lyrics.map((lyric, ind) => <span key={ind} >{lyric}<br /></span>)}
-          //     </p>
-          //   </section>
-          // </section>
         ) : (
           <section className="disclaimer">
             <h2>{ t('disclaimer.header') }</h2>
