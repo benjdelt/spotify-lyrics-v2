78bbf0592556da32dff25961028c87105a4371d3
Ben Deltenre <benjdelt@gmail.com>
2021-05-11 07:08:59 -0700

    Styled error messages

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
+import '../styles/Error.css';
+
 function Error({error}) {
   return (
     <section className="error">
-      <p>{error}</p>
+      <p>
+        <FontAwesomeIcon icon={faExclamationCircle} />
+        <span>&nbsp;{error}</span>
+      </p>
     </section>
   );
 }
