58344e86a9a89e8df05ed9768d3ec1ab6b442553
Ben Deltenre <benjdelt@gmail.com>
2021-05-14 08:00:56 -0700

    cleaned title with regex

--- functions/lyrics.js
+++ functions/lyrics.js
@@ -1,9 +1,20 @@
 const axios = require('axios');
 
 exports.handler = async function(event, context) {
+  
+  function cleanTitle(title) {
+    const regex = /^(.+)\s-+?.*$/i; // Grabs everything before space + dash
+    const result = title.match(regex);
+    if (result) {
+        return result[1];
+    }
+    return title;
+  }
+
   const artist = event.queryStringParameters.artist;
   const title = event.queryStringParameters.title;
-  const uri = `https://api.lyrics.ovh/v1/${artist}/${title}`
+  const uri = `https://api.lyrics.ovh/v1/${artist}/${cleanTitle(title)}`
+
   try {
     const response = await axios.get(uri)
     return {
